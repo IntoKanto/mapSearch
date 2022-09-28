9c18c4d4f1213b079d399e7eb6a84df295965998
IntoKanto <anatoli.tselms@myy.haaga-helia.fi>
2022-09-28 21:21:43 +0300

    map_search

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import * as Location from 'expo-location';
+import React, { useEffect, useState, useSyncExternalStore } from 'react';
 
 export default function App() {
+  const [input, setInput] = useState('')
+  const [location, setLocation] = useState({
+    latitude: '',
+    longitude: ''
+  })
+  
+  
+
+  const show = () => {
+    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=h0xO5elplIMHHi7YC1HGtkuyfedDwuxS&location=${input.replaceAll(' ', '')}`)
+    .then(response => response.json())
+    .then(data => {
+      setLocation({
+        latitude: data.results[0].locations[0].latLng.lat,
+        longitude: data.results[0].locations[0].latLng.lng
+      })
+    })
+  }
+
+
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    
+   
+
+   <React.Fragment>
+    <MapView style={{flex: 1}}
+    region ={{
+      latitude: location.latitude,
+      longitude: location.longitude,
+      latitudeDelta: 0.0322,
+      longitudeDelta: 0.0221
+    }} >
+    <Marker
+      title={input}
+      coordinate={{
+        latitude: location.latitude,
+        longitude: location.longitude
+      }}
+
+    />
+  
+    </MapView>
+    <TextInput style={styles.txtinput}
+    value={input}
+    onChangeText={text => setInput(text)}
+    />
+    
+   <Button 
+   title="Show on map"
+   onPress={show}
+   /> 
+  
+   </React.Fragment>
+   
+   
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +72,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txtinput: {
+    borderWidth: 1,
+    borderColor: 'blue',
+    width: 150
+  },
+  map: {
+    flex: 1
+  }
 });
